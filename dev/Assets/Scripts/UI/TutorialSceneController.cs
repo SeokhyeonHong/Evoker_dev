5b3c395dc09b86398da08425cb1918ea7e50378b
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2021-12-09 19:44:56 +0900

    Add background story scene

--- dev/Assets/Scripts/UI/TutorialSceneController.cs
+++ dev/Assets/Scripts/UI/TutorialSceneController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class TutorialSceneController : MonoBehaviour
 {
     public List<Sprite> ImgList = new List<Sprite>();
-    public GameObject StartButton;
+    public GameObject StartButton, PrevButton, NextButton;
     private int m_ImgNum = 0;
     void Start()
     {
@@ -13,8 +14,12 @@ public class TutorialSceneController : MonoBehaviour
 
     void Update()
     {
-        bool active = m_ImgNum < ImgList.Count - 1 ? false : true;
-        StartButton.SetActive(active);
+        bool is_first_page = m_ImgNum == 0 ? true : false;
+        bool is_last_page = m_ImgNum == ImgList.Count - 1 ? true : false;
+        StartButton.SetActive(is_last_page);
+        NextButton.SetActive(!is_last_page);
+        PrevButton.SetActive(!is_first_page);
+        this.GetComponent<Image>().sprite = ImgList[m_ImgNum];
     }
 
     public void IncreaseImgNum()
